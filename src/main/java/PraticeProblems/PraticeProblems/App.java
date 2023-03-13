854dd25c02bf327f5da068163b47d7de8bf6ea9b
nehal19 <nehalbk19@gmail.com>
2023-03-13 09:31:22 +0530

    Repo Setup

---
+++ src/main/java/PraticeProblems/PraticeProblems/App.java
@@ -0,0 +1,13 @@
+package PraticeProblems.PraticeProblems;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
